9d3dce19adeab69d8ae9c981c413adbfbc19ddbb
Riemannulus Allons-y <riemannulus@hitagi.moe>
2019-04-25 10:27:57 +0900

    Initialization

---
+++ osm2fbx/osm/module.py
@@ -0,0 +1,6 @@
+from imposm.parser import OSMParser
+
+class OSMmanager():
+    __node_coordinate = {}
+
+    def __init__(self, path):
---
+++ setup.sh
@@ -0,0 +1,24 @@
+#!/bin/env bash
+PYTHONPATH=$(which python)
+PYTHONBIN="$(dirname "$PYTHONPATH")"
+PYTHONHOME="$(dirname "$PYTHONBIN")"
+PYTHONLIB=$PYTHONHOME"/lib"
+
+echo "==========================================================================="
+echo "PYTHONPATH: "$PYTHONPATH
+echo "PYTHONBIN: "$PYTHONBIN
+echo "PYTHONHOME: "$PYTHONHOME
+echo "PYTHONLIB: "$PYTHONLIB
+echo "==========================================================================="
+
+mkdir .tmp && cd .tmp
+wget http://download.autodesk.com/us/fbx/2019/2019.0/fbx20190_fbxpythonsdk_linux.tar.gz
+tar -xvzf fbx20190_fbxpythonsdk_linux.tar.gz
+yes yes | ./fbx20190_fbxpythonsdk_linux
+cp ./lib/Python27_ucs4_x64/* $PYTHONLIB"/python2.7"
+cd .. && rm -rf .tmp
+
+
+echo "==========================================================================="
+echo " INSTALL FINISHED"
+echo "==========================================================================="
